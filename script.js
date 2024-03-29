d9526d897f52afea13d8f379fba06da849af9ed7
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2024-03-29 19:36:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("from script file");
 
+var head = document.getElementById('head');
+head.style.background = 'rgba(255, 165, 0, 000)';
+
 document.addEventListener('scroll', function() {
     var scrolled = window.pageYOffset;
     var rate = scrolled * -0.2; // Adjust the 0.5 to control the speed of the parallax effect
@@ -7,4 +10,25 @@ document.addEventListener('scroll', function() {
     document.querySelector('.sunset').style.backgroundPosition = 'center ' + rate + 'px';
   });
   
-  
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarTitle = document.getElementById('navbar-title');
+    var bigTitle = document.getElementById('title-text');
+    var scrollAmount = 320; // The amount of pixels scrolled after which to show the element
+    
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > scrollAmount) { //Scroll down
+            navbarTitle.style.opacity = '1'; // Fade in
+            bigTitle.style.opacity = '0'; // Fade out
+            head.style.background = 'linear-gradient(to bottom, #BC7FB6 0%, #70488D 100%)';
+        } else { //Scroll up
+            navbarTitle.style.opacity = '0'; // Fade out
+            bigTitle.style.opacity = '1'; // Fade in
+            head.style.background = 'rgba(255, 165, 0, 000)';
+        }
+    });
+});
+
+function toggleMenu(){
+  var menu = document.getElementById('menu');
+  menu.classList.toggle('show');
+}
